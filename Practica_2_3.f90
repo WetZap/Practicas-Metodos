8dd8436207ee30a95a8a8bdde5eadb147982af07
WetZap <88139492+WetZap@users.noreply.github.com>
2024-04-10 06:36:22 +0200

    Add files via upload

---
+++ Practica_2_3.f90
@@ -0,0 +1,35 @@
+function funcion(x) result(valor)
+    implicit none
+    real*8 :: x,valor
+    valor=1.d0/(1.d0+x)
+end function
+
+program Practica_2_3
+    implicit none
+    real*8 a,b,h,suma_1,suma_2,integral,stop
+    real*8,external::funcion
+    integer n,k
+    !Inicializo los limites
+    a=0.d0
+    b=1.d0
+    !Pregunto el valor de n
+    print*,"Valor n:"
+    read(*,*)n
+    !Defino h
+        h=(b-a)/n
+    suma_1=0.d0
+    !Hacemos las dos que nos dicen
+    do k=2,(n/2)
+        suma_1=suma_1+funcion(a+((2*k-2)*h))
+    end do  
+
+    suma_2=0.d0
+    do k=1,(n/2)
+        suma_2=suma_2+funcion(a+((2*k-1)*h))
+    end do
+
+    integral=h/3*( (1.d0/(1.d0+a)) + (1.d0/(1.d0+b)) + 2*suma_1 + 4*suma_2)
+    print *,"La integral es: ",integral
+    read(*,*) stop
+    
+end program Practica_2_3
