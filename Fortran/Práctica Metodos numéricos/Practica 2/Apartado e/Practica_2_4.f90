d4ec73f2c6130798f2a2eb2d6af44217a7479a56
WetZap <88139492+WetZap@users.noreply.github.com>
2024-04-11 23:12:42 +0200

    Update Practica_2_4.f90

--- Fortran/Práctica Metodos numéricos/Practica 2/Apartado e/Practica_2_4.f90
+++ Fortran/Práctica Metodos numéricos/Practica 2/Apartado e/Practica_2_4.f90
@@ -14,17 +14,23 @@ program Practica_2_4
     b=1.d0
     print*,"Valor de n: "
     read(*,*)n
-    !Aplicamos la formula del calculo integral
-    h=b-a
-    x(0)=h/2*(funcion(a)+funcion(b))
-    do i=1,n
-        h=h/2
-        suma=0.d0
-        do j = 1, 2**(i-1)
-            suma=suma+funcion(a+(2*j-1)*h)
+    if ( mod(n,2)==0 ) then
+        !Aplicamos la formula del calculo integral
+        h=b-a
+        !Declaramos la funcion en el 0 para iniciar el bucle desde 1 hasta n
+        x(0)=h/2*(funcion(a)+funcion(b))
+        do i=1,n
+            !Conseguimos dividir el valor de h entre 2 para que sea el correspondiente a cada valor de h
+            h=h/2
+            suma=0.d0!Inicializamos la suma a 0 para que la suma resultante no se vea afectada
+            do j = 1, 2**(i-1)
+                suma=suma+funcion(a+(2*j-1)*h)
+            end do!Escribimos el valor de x(i) como la formula empleando la anterior.
+            x(i)=x(i-1)/2+h*suma
         end do
-        x(i)=x(i-1)/2+h*suma
-    end do
-    print*,"El valor de la integral es: ",x(n)
+        print*,"El valor de la integral es: ",x(n)
+    else
+        print*,"El valor de n debe ser un multiplo de 2."
+    end if
     read(*,*) stop
 end program Practica_2_4
