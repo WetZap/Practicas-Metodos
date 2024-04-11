f941a58e5e97dd7490741380e6f5c5ad2fb64262
WetZap <88139492+WetZap@users.noreply.github.com>
2024-04-11 23:36:40 +0200

    Update Practica_2.0.f90

--- Fortran/Práctica Metodos numéricos/Practica 2/Apartado a/Practica_2.0.f90
+++ Fortran/Práctica Metodos numéricos/Practica 2/Apartado a/Practica_2.0.f90
@@ -2,12 +2,15 @@ program Practica_2_0
     implicit none
     real*8 Coeficientes_cotes(0:9,8)
     integer i,j,valor
+    !Abrimos el archivo en que queremos guardar los coeficientes
     open(11,file="Coeficientes_cotes_nuevo.txt",status="unknown")
+    !La variable i significa columnas y la j las filas
     do i = 0,9
         do j = 1,8
             read(*,*) valor
-            Coeficientes_cotes(i,j)=valor
+            Coeficientes_cotes(i,j)=valor!Le pregunto el valor que quiere introducir.
         end do
     end do
+    !Escribo la matriz resultante en archivo.
     write (11,*)Coeficientes_cotes
 end program Practica_2_0
