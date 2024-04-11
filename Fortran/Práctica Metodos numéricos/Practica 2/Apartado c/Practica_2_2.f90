6845286211a7adfcfd67b428bad5cc5559e4d6ff
WetZap <88139492+WetZap@users.noreply.github.com>
2024-04-11 23:36:19 +0200

    Update Practica_2_2.f90

--- Fortran/Práctica Metodos numéricos/Practica 2/Apartado c/Practica_2_2.f90
+++ Fortran/Práctica Metodos numéricos/Practica 2/Apartado c/Practica_2_2.f90
@@ -7,7 +7,7 @@ end function
 program Practica_2_2
     implicit none
     real*8 a,b,h,suma,integral,stop
-    real*8 , external::funcion
+    real*8 , external::funcion!Esto sirve para que el programa entienda las funciones
     integer n,k
     !Declaramos las variables que tomaran valores
     a=0.d0
@@ -19,7 +19,7 @@ program Practica_2_2
     suma=0.d0
 
     do k=1,(n-1)
-        suma=suma+funcion(a+k*h)
+        suma=suma+funcion(a+k*h)!Es a +k*h, porque kh es el incremento del intervalo
         print*,suma
     end do 
     
