754a354ecafba15c4231936636faf5aced98265a
WetZap <88139492+WetZap@users.noreply.github.com>
2024-04-11 23:35:54 +0200

    Update Practica_2_5.f90

--- Fortran/Práctica Metodos numéricos/Practica 2/Apartado f/Practica_2_5.f90
+++ Fortran/Práctica Metodos numéricos/Practica 2/Apartado f/Practica_2_5.f90
@@ -4,28 +4,26 @@ function funcion(x) result(valor)!Esta funcion es la funcion que nos piden integ
     valor=1.d0/(1.d0+x)
 end function
 
-function funcion_fi(x,h) result(valor)
-    implicit none
+function funcion_fi(x,h) result(valor)!Lo que hace esta funcione es calcular los valores de la funcion introducida apoyandose en la 
+    implicit none!funcion anterior
     real*8 :: x,valor,h,funcion
-    valor=(funcion(x+h)-funcion(x-h))/2*h
+    valor=(funcion(x+h)-funcion(x-h))/(2*h)
 end function
 
 program Practica_2_5
     implicit none
     real*8 x,h,f_prima_tay,f_prima_rich,stop
-    real*8,external :: funcion, funcion_fi
-    ! Inicializo las variables
+    real*8,external :: funcion, funcion_fi!Esto sirve para que el programa entienda las funciones
+    ! Inicializo las variables y le pido que introduzca las variables
     print*,"Valor de x: "
     read(*,*)x
     print*,"Valor de h: "
     read(*,*)h
-    x=0.d0
-    h=0.0001
    !Primera forma, desarrollo en serie de Taylor
-    f_prima_tay=(funcion(x+h)-funcion(x-h))/(2.d0*h)
+    f_prima_tay=funcion_fi(x,h)
 
    !Segunda forma, extrapolacion de Richardson
-    f_prima_rich=((4.d0/3.d0)*(funcion(x+(h/2.d0))-funcion(x-(h/2.d0)))/h)-(1.d0/3.d0)*((funcion(x+h)-funcion(x-h))/(2*h))
+    f_prima_rich=(4.d0/3.d0)*(funcion_fi(x,h/2))-(1.d0/3.d0)*(funcion_fi(x,h))
     
     !Expreso los valores
     print*,"El valor según el desarrollo en serie de Taylor es: ",f_prima_tay
