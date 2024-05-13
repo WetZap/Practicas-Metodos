ee0ae26fe487efaacdcf4978d0e35c96c4ef0106
Jorge <jorgerlvdg2016@gmail.com>
2024-05-13 14:46:08 +0200

    Practica B Con comentarios

--- Fortran/Práctica Metodos numéricos/Practica 4/Apartado b/Biseccion/Practica_4_b.f90
+++ Fortran/Práctica Metodos numéricos/Practica 4/Apartado b/Biseccion/Practica_4_b.f90
@@ -1,34 +1,42 @@
-function funcion(x) result(valor)
+function funcion(x) result(valor)!Funcion que toma el valor de la función deseada.
     implicit none
     real*8 x,valor
-    valor=x**3-5*x**2+7*x-3
+    valor= x**3- 5*x**2 + 7*x - 3
 end function
 
 program Practica_4_Biseccion
     implicit none
     real*8,external::funcion
-    real*8 a,b,valor,error,c,difere,f_a,f_b,c_anterior
+    real*8 a,b,valor,error,c,difere,f_a,f_b,c_anterior!Definimos las variables
+    !Definimos los valores de los limites del intervalo.
     a=0.d0
     b=5.d0
-    error=10.d0**(-3)
-    difere=1.d0
-    c=(b+a)/(2.d0)
-    c_anterior=c
-    do while(difere>=error)
-        if (funcion(c)*funcion(a)>0) then
-            a=c
-            c=(b+a)/(2.d0)
-            difere=abs((funcion(c))-(funcion(c_anterior)))
-            c_anterior=c
-        else if(funcion(c)*funcion(a)<0)then
-            b=c
-            c=(b+a)/(2.d0)
-            print*,c
-            difere=abs((funcion(c))-(funcion(c_anterior)))
-            c_anterior=c
-        end if
-         if(funcion(c)*funcion(a)==0) exit 
-    end do
+    if ( funcion(a)*funcion(b)<0 ) then!Comprobamos que la funcion tiene un cero.
+        !Definimos el error que consideraremos.
+        error=10.d0**(-3)
+        difere=1.d0
+        !Definimos el valor de c como la SUMA de los intervalos entre 2.
+        c=(b+a)/(2.d0)
+        c_anterior=c
+        !Comenzamos el bucle que se encargara de que llegar al error deseado.
+        do while(difere>=error)
+            if (funcion(c)*funcion(a)>0) then!Esta sería la primera condicion del método.
+                a=c!Cambiamos el valor de a por el de c ya que nos interesa estudiar el intervalo [c,b]
+                c=(b+a)/(2.d0)!Volvemos a definir el valor de c y comprobamos la diferencia de este con el anterior.
+                difere=abs((funcion(c))-(funcion(c_anterior)))
+                c_anterior=c
+            else if(funcion(c)*funcion(a)<0)then
+                b=c!Lo mismo que arriba
+                c=(b+a)/(2.d0)
+                difere=abs((funcion(c))-(funcion(c_anterior)))!
+                c_anterior=c
+            end if
+            if(funcion(c)*funcion(a)==0) exit !Cuando sea 0 se marcha.
+        end do
+        !Imprimimos el valor del sitio del 0.
+        print*,"El valor ",c,"es un cero de la función."
+    else
+        print*,'La funcion no pasa de postivo a negativo en el intervalo escogido.'
+    end if
 
-    print*,"El valor ",c,"es un cero de la función."
 end program Practica_4_Biseccion
--- Fortran/Práctica Metodos numéricos/Practica 4/Apartado b/Regula Falsi/Practica_4_b.f90
+++ Fortran/Práctica Metodos numéricos/Practica 4/Apartado b/Regula Falsi/Practica_4_b.f90
@@ -1,36 +1,35 @@
-function funcion(x) result(valor)
+function funcion(x) result(valor)!Funcion que toma el valor de la función deseada.
     implicit none
     real*8 x,valor
-    valor=x**3-5*x**2+7*x-3
-
+    valor= x**3- 5*x**2 + 7*x - 3
 end function
 
 program Practica_4_Regula
     implicit none
     real*8,external::funcion
-    real*8 a,b,valor,error,c,difere,f_a,f_b,c_1,i
+    real*8 a,b,valor,error,c,difere,f_a,f_b,c_1
+    !Definimos los valores de los limites del intervalo.
     a=0.d0
     b=5.d0
+    !Definimos el error.
     error=10.d0**(-3)
     difere=1.d0
+    !Tomamos el valor de c como la formula descrita en clase.
     c=(a*funcion(b)-b*funcion(a))/(funcion(b)-funcion(a))
     c_1=c
-    do while(difere>=error)
-        i=i+1
-        if (funcion(c)*funcion(a)>0) then
-            a=c
-            c=(a*funcion(b)-b*funcion(a))/(funcion(b)-funcion(a))
-            difere=abs(abs(funcion(c))-abs(funcion(c_1)))
+    do while(difere>=error)!Comenzamos nuestro bucle.
+        if (funcion(c)*funcion(a)>0) then!Hacemos lo mismo que hicimos en Biseccion.
+            a=c!Cambiamos el valor de a por el de c ya que nos interesa estudiar el intervalo [c,b]
+            c=(a*funcion(b)-b*funcion(a))/(funcion(b)-funcion(a))!Volvemos a definir el valor de c y comprobamos la diferencia 
+            difere=abs(abs(funcion(c))-abs(funcion(c_1)))!de este con el anterior.
         else if(funcion(c)*funcion(a)<0)then
-            b=c
+            b=c!lo mismo pero con el intervalo [a,c]
             c=(a*funcion(b)-b*funcion(a))/(funcion(b)-funcion(a))
             difere=abs(abs(funcion(c))-abs(funcion(c_1)))
-        else if(funcion(c)*funcion(a)==0)then 
-            go to 100
         end if
+        if(funcion(c)*funcion(a)==0) exit !Si encontramos el cero nos vamos.
         c_1=c
     end do
-
+    !Imprimimos el resultado.
     print*,"El valor ",c,"es un cero de la función."
-    100 print*,"El valor ",c,"es un cero de la función."
 end program Practica_4_Regula
