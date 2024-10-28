64560cd479fa1b3a043745f34be52bd997c7f744
Jorge <jorgerlvdg2016@gmail.com>
2024-10-28 09:24:05 +0100

    Preparacion Publico

---
+++ Práctica Metodos numéricos/Practica 2/Apartado e/Practica_2_4.f90
@@ -0,0 +1,39 @@
+function funcion(x) result(valor)!Esta funcion es la funcion que nos piden integrar
+    implicit none
+    real*8 :: x,valor
+    valor=x/((1+x**2)**3)
+end function
+
+program Practica_2_4
+    implicit none
+    real*8 x(0:19),h,suma,a,b,stop
+    real*8,external::funcion!Esto sirve para que el programa entienda las funciones
+    integer i,j,n
+    !Declaramos las variables
+    a=0.d0
+    b=1.d0
+    print*,"Valor de n: "
+    read(*,*)n
+    !Comprobamos que el valor introducido se un multiplo de 2.
+    if ( mod(n,2)==0 ) then
+        !Aplicamos la formula del calculo integral
+        h=b-a
+        !Declaramos la funcion en el 0 para iniciar el bucle desde 1 hasta n
+        x(0)=h/2*(funcion(a)+funcion(b))
+        do i=1,n
+            !Conseguimos dividir el valor de h entre 2 para que sea el correspondiente a cada valor de h
+            h=h/2
+            suma=0.d0!Inicializamos la suma a 0 para que la suma resultante no se vea afectada
+            do j = 1, 2**(i-1)
+                suma=suma+funcion(a+(2*j-1)*h)
+            end do!Escribimos el valor de x(i) como la formula empleando la anterior.
+            x(i)=x(i-1)/2+h*suma
+        end do
+        print*,"Para una n=",n,":"
+        print*,"El valor de la integral es: ",x(n)
+        print*,"La diferencia es: ",0.187500000-x(n)
+    else
+        print*,"El valor de n debe ser un multiplo de 2."
+    end if
+    read(*,*) stop
+end program Practica_2_4
